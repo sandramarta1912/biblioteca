867f0d14e7c982890cec254908f1c8b38c327870
Corneliu Vesa <vesacorneliu@yahoo.com>
2017-10-27 16:03:51 +0300

    Update sermon.controller.js

--- app/preacher/sermon.controller.js
+++ app/preacher/sermon.controller.js
@@ -1,7 +1,10 @@
 module.exports = {
     showSermons: (req, res) => {
+        // TODO What kind o route is this?
         res.render('pages/preacher & sermon/sermons.pug', {
             // csrfToken: req.csrfToken()
         });
     }
+    
+    // TODO Have a look in preachers.controller.js and implement all those handlers in here, also
 };
