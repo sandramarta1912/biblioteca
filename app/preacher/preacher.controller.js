56d787b9d35087b9a8b5c1eeb654bbcc03f94e43
Corneliu Vesa <vesacorneliu@yahoo.com>
2017-10-27 16:01:46 +0300

    Update preacher.controller.js

--- app/preacher/preacher.controller.js
+++ app/preacher/preacher.controller.js
@@ -2,8 +2,15 @@ module.exports = {
 
     //show the body.pug page
     showPreacher: (req, res) => {
+        // TODO What route is this?
         res.render('pages/preacher & sermon/preacher.pug', {
             // csrfToken: req.csrfToken()
         });
     }
+    
+    // TODO Add create preacher handler
+    
+    // TODO Add edit preacher handler
+    
+    // TODO Add delete preacher handler
 };
